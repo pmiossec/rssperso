1e95fd2fcd2cc18fbfa9633f6a6a8a5609f1486e
Philippe Miossec <pmiossec@gmail.com>
2017-01-22 16:33:00 +0100

    initial version
    using http://fountainjs.io/doc/usage (option 'landing-page')

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import 'es6-promise';
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import {Main} from './app/main';
+
+import './index.scss';
+
+ReactDOM.render(
+  <Main/>,
+  document.getElementById('root')
+);
