089c8329cbfa6dc67206995441ccdf8b6d64fc70
Philippe Miossec <pmiossec@gmail.com>
2017-03-11 12:52:34 +0100

    link command otherwise cmd cannot find the bat file that is only in master
    error: "The batch file cannot be found."

--- to_ghpages.bat
+++ to_ghpages.bat
@@ -3,6 +3,4 @@ git commit -am "Save local changes"
 git checkout -B gh-pages
 git add -f dist
 git commit -am "Rebuild website"
-git filter-branch -f --prune-empty --subdirectory-filter dist
-git push -f origin gh-pages
-git checkout -
+git filter-branch -f --prune-empty --subdirectory-filter dist && git push -f origin gh-pages && git checkout -
