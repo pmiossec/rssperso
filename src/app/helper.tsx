c46a251716d9db59de947e6b3902ed423a9fe0b4
Philippe Miossec <pmiossec@gmail.com>
2017-10-20 20:21:31 +0200

    Add support to read data from a GitHub gist

--- src/app/helper.tsx
+++ src/app/helper.tsx
@@ -1,5 +1,6 @@
 import { Link } from './feeds/news';
 import { BlobRemoteStorage } from './readingList/blobRemoteStorage';
+import { GistRemoteStorage } from './readingList/gistRemoteStorage';
 
 export namespace DateFormatter {
   const padDigits = (num: number, digits: number = 2): string => {
@@ -20,8 +21,56 @@ export namespace DateFormatter {
 export const ReadingListKey: string = 'ReadingList';
 export const ArchiveListKey: string = 'ArchiveList';
 
+interface Gist {
+  feeds: FeedData[];
+  state: State;
+  readList: ReadListItem[];
+}
+
+interface FeedData {
+  id: string;
+  name: string;
+  url: string;
+  icon: string;
+}
+interface Feeds {
+  feeds: FeedData[];
+}
+
+interface FeedState {
+  id: string;
+  date: Date | null;
+}
+
+interface State {
+  last_update: Date;
+  updates: { [feedid: string]: FeedState};
+}
+
+interface ReadListItem {
+  id: string | null;
+  title: string;
+  url: string;
+  publicationDate: Date;
+}
+
+interface GistFileContent {
+  filename: string;
+  type: string;
+  language: string;
+  raw_url: string;
+  size: string;
+  truncated: string;
+  content: string;
+}
+
+interface GistStorage {
+  files: { [fileId: string]: GistFileContent; };
+}
+
 export namespace Storage {
 
+  const gistStorage: GistRemoteStorage = new GistRemoteStorage();
   const readListStorage: BlobRemoteStorage = new BlobRemoteStorage('4013cc1f-0657-11e7-a0ba-8deb10d7638f');
   const archiveListStorage: BlobRemoteStorage = new BlobRemoteStorage('1a115a19-06a3-11e7-a0ba-399ae9b7d9b1');
   var stateChanged: boolean = true;
@@ -39,7 +88,28 @@ export namespace Storage {
     return archiveListStorage;
   };
 
-  const loadReadingList = (storeName: string): Promise<Link[]> => {
+  const loadGist = (): Axios.IPromise<Gist> => {
+    return gistStorage.getDataFromRemote().then((data: GistStorage) => {
+      var feeds = (JSON.parse(data.files['feed.json'].content) as Feeds).feeds;
+      var state = JSON.parse(data.files['state.json'].content) as State;
+      var readList = JSON.parse(data.files['readlist.json'].content) as ReadListItem[];
+      // var archive = JSON.parse(data.files['archive.json'].content) as ReadList;
+      return {feeds, state, readList};
+    });
+  };
+
+  const loadReadingList = async (storeName: string): Promise<Link[]> => {
+    const gist = await loadGist();
+    // tslint:disable
+    console.log('gist', gist);
+
+    console.log(gist.readList[9].title);
+    console.log(gist.feeds[9].id);
+    console.log(gist.state.last_update);
+    console.log(gist.state.updates[gist.feeds[9].url]);
+
+    // tslint:enable
+
     var p = new Promise<Link[]>((resolve, reject) => {
       const remoteStore = GetStore(storeName);
       if (!remoteStore.dataFetched) {
@@ -65,6 +135,7 @@ export namespace Storage {
   };
 
   export const loadReadingListIfChanged = (storeName: string): Promise<Link[]> => {
+
     if (!stateChanged) {
       return new Promise<Link[]>((resolve, reject) => { resolve(Link[0]); });
     }
