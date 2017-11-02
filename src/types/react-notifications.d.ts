0a93e84bdf128b9b19b68d0bf14d141342992313
Philippe Miossec <pmiossec@gmail.com>
2017-11-02 13:15:06 +0100

    Save local changes

--- src/types/react-notifications.d.ts
+++ src/types/react-notifications.d.ts
@@ -1,2 +1,13 @@
 declare module 'react-notifications';
 
+
+
+// export interface NotificationManager {
+//     info(message: string, title: string, timeOut: number, callback: ()=> void, priority: string): void;
+//     success(message: string, title: string, timeOut: number, callback: ()=> void, priority: string): void;
+//     warning(message: string, title: string, timeOut: number, callback: ()=> void, priority: string): void;
+//     error(message: string, title: string, timeOut: number, callback: ()=> void, priority: string): void;
+// }
+
+
+
